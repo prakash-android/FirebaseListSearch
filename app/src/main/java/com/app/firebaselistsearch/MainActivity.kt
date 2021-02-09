c824f700199c2bf23fe8e20071d643580f42edde
Prakash android <58256692+prakash-android@users.noreply.github.com>
2021-02-09 18:57:40 +0530

    almost done

--- app/src/main/java/com/app/firebaselistsearch/MainActivity.kt
+++ app/src/main/java/com/app/firebaselistsearch/MainActivity.kt
@@ -2,10 +2,37 @@ package com.app.firebaselistsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUI()
     }
+
+    private fun initUI() {
+        //firestore
+        val db = Firebase.firestore
+
+        //read data
+        db.collection("Users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.e("TAG", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.e("TAG", "Error getting documents.", exception)
+            }
+    }
+
+
+
+
+
 }
