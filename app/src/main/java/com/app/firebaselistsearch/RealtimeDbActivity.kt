1fdf96bafef3e3a5d2d76d1cfc9bc255cd54f86e
Prakash android <58256692+prakash-android@users.noreply.github.com>
2021-02-10 18:47:03 +0530

    almost done

--- app/src/main/java/com/app/firebaselistsearch/RealtimeDbActivity.kt
+++ app/src/main/java/com/app/firebaselistsearch/RealtimeDbActivity.kt
@@ -2,6 +2,7 @@ package com.app.firebaselistsearch
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,7 +10,6 @@ import com.app.firebaselistsearch.adapter.MyUserAdapter
 import com.app.firebaselistsearch.model.MyUser
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
-import com.google.firebase.firestore.auth.User
 import com.google.firebase.ktx.Firebase
 
 
@@ -19,7 +19,9 @@ class RealtimeDbActivity : AppCompatActivity(), MyUserAdapter.UpcomingAdapterLis
     lateinit var myUserAdapter: MyUserAdapter
     var userArrayList: ArrayList<MyUser> = ArrayList()
     lateinit var recyclerView: RecyclerView
-    private lateinit var database: DatabaseReference
+    lateinit var mDatabase: DatabaseReference
+    lateinit var mMessageReference: DatabaseReference
+    lateinit var mMessageListener: ChildEventListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,17 +30,84 @@ class RealtimeDbActivity : AppCompatActivity(), MyUserAdapter.UpcomingAdapterLis
     }
 
     private fun initUI() {
+        //recyclerview
+        recyclerView = findViewById<RecyclerView>(R.id.result_list)
 
-        database = Firebase.database.reference
+        mDatabase = Firebase.database.reference
 
-        var dataref = database.child("data")
-        Log.e("res", "" + dataref.toString())
+         mMessageReference = mDatabase.child("data")
 
+        val childEventListener: ChildEventListener = object : ChildEventListener {
+            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                // A new message has been added
+                // onChildAdded() will be called for each node at the first time
+                val myUser: MyUser? = dataSnapshot.getValue(MyUser::class.java)
+                userArrayList.add(myUser!!)
+                Log.e("TAG", "onChildAdded:" + myUser!!.first_name)
+//                val latest: MyUser = messageList.get(messageList.size() - 1)
+//                tvAuthor.setText(latest.author)
+//                tvTime.setText(latest.time)
+//                tvBody.setText(latest.body)
+            }
 
+            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.e("TAG", "onChildChanged:" + dataSnapshot.key)
 
+                // A message has changed
+                val myUser: MyUser? = dataSnapshot.getValue(MyUser::class.java)
+                Toast.makeText(
+                    this@RealtimeDbActivity,
+                    "onChildChanged: " + myUser!!.first_name,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {
+                Log.e("TAG", "onChildRemoved:" + dataSnapshot.key)
+
+                // A message has been removed
+                val myUser: MyUser? = dataSnapshot.getValue(MyUser::class.java)
+                Toast.makeText(
+                    this@RealtimeDbActivity,
+                    "onChildRemoved: " + myUser!!.first_name,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.e("TAG", "onChildMoved:" + dataSnapshot.key)
+
+                // A message has changed position
+                val myUser: MyUser? = dataSnapshot.getValue(MyUser::class.java)
+                Toast.makeText(
+                    this@RealtimeDbActivity,
+                    "onChildMoved: " + myUser!!.first_name,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("TAG", "postMessages:onCancelled", databaseError.toException())
+                Toast.makeText(this@RealtimeDbActivity, "Failed to load Message.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        mMessageReference.addChildEventListener(childEventListener)
+        // copy for removing at onStop()
+        mMessageListener = childEventListener
+
+        setupRecyclerView()
 
     }
 
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mMessageListener != null) {
+            mMessageReference.removeEventListener(mMessageListener)
+        }
+    }
 
     private fun setupRecyclerView() {
         if(userArrayList.isEmpty()){
--- app/src/main/java/com/app/firebaselistsearch/model/MyData.kt
+++ app/src/main/java/com/app/firebaselistsearch/model/MyData.kt
@@ -1,5 +1,7 @@
 package com.app.firebaselistsearch.model
 
+import com.google.firebase.database.Exclude
+import com.google.firebase.database.IgnoreExtraProperties
 
 
 data class Users(
@@ -9,10 +11,43 @@ data class Users(
 )
 
 
-data class MyUser(
-    val id : String,
-    val first_name: String,
-    val last_name: String,
-    val email: String,
-    val avater: String
-)
+//data class MyUser(
+//    val id: String,
+//    val first_name: String,
+//    val last_name: String,
+//    val email: String,
+//    val avater: String
+//)
+
+
+@IgnoreExtraProperties
+class MyUser {
+    var id: String? = null
+    var first_name: String? = null
+    var last_name: String? = null
+    var email: String? = null
+    var avater: String? = null
+
+    constructor() {
+        // Default constructor required for calls to DataSnapshot.getValue(Post.class)
+    }
+
+    constructor(id: String?, first_name: String?, last_name: String?, email: String?, avater: String?) {
+        this.id = id
+        this.first_name = first_name
+        this.last_name = last_name
+        this.email = email
+        this.avater = avater
+    }
+
+    @Exclude
+    fun toMap(): Map<String, Any?> {
+        val result: HashMap<String, Any?> = HashMap()
+        result["id"] = id
+        result["first_name"] = first_name
+        result["last_name"] = last_name
+        result["email"] = email
+        result["avater"] = avater
+        return result
+    }
+}
